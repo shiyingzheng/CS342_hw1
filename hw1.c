5f8aa8d5829663118c01b0608e5d5aa2aad7c5b1
Ben Stern <bstern@cylon.cs.oberlin.edu>
2015-09-12 15:50:19 -0400

    hw1

--- hw1.c
+++ hw1.c
@@ -3,8 +3,25 @@
 int write_buffer(char* buf, FILE* fptr) {
 }
 
+/*
+ * error codes:
+ * 1 omitted url
+ * 2 malformed url
+ * 3 unknown host
+ * 4 server did not respond
+ * 5 server did not respond with a header
+ * 6 or greater got error code errcode
+ */
+int print_error(int errcode) {
+    
+}
+
 int main(int argc, char** argv){
+    if (argc < 2) {
+        print_error(1);
+    }
     char host[100];
     char path[100];
     char file[100];     
+    "meowmeowmeow // meowmeowmeow" // a comment
 }
