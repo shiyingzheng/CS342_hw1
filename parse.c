3eefe0280d149483072cda949097845ef3ac860a
Shiying Zheng <szheng@dalek.cs.oberlin.edu>
2015-09-12 14:11:15 -0400

    ayy lmao

---
+++ parse.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<regex.h>
+
+int main( int argc, char *argv[] )
+{
+    url = argv[0];
+    protocol = regcomp(&url, "", 0);
+    website = regcomp();
+    resource = regcomp();
+}
